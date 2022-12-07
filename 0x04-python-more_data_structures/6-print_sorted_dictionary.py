2dfb8d57d960657ec12555e84c488bb4a1eb050e
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-07 14:54:03 -0500

    Fix bug

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     ordered = sorted(a_dictionary)
     for i in ordered:
-        print("{}: {}".format(i, a_dicionary[i]))
+        print("{}: {}".format(i, a_dictionary[i]))
