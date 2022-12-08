ac5a7c8b6d5c3de78d9387037e788a7fec74cc70
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-08 04:59:57 -0500

    Update dictionary numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000,\
-	    'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
-
-    if roman_string and type(roman_string) == str:
-        num = 0
-        for i in range(len(roman_string)):
-            for j in romans.keys():
-                if roman_string[:] is j:
-                    num = romans[j]
-                    return num
-                if roman_string[i:i+1] is j:
-                    num += romans[j]
-                    break
-        return num
+    if isinstance(roman_string, str):
+        result = 0
+        numb_convert = 0
+        dict_numbers = {"I": 1, "V": 5, "X": 10, "L": 50, \
+                        "C": 100, "D": 500, "M": 1000, "IV": 4, \
+                        "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900
+                        }
+        
+        for i in reversed(roman_string):
+            numb_convert = dict_numbers[i]
+            if result < numb_convert * 5:
+                result = result + numb_convert
+            else:
+                result = result - numb_convert
+        return result
     else:
         return 0
