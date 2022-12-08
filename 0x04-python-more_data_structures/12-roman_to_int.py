7c3610771498a070d864a4dfc2cd290b38b4a4bb
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-08 04:44:35 -0500

    Deploy new algorithm

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str):
-        result = 0
-        numb_convert = 0
-        dict_numbers = {"I": 1, "V": 5, "X": 10, "L": 50,
-                        "C": 100, "D": 500, "M": 1000}        
-        for i in reversed(roman_string):
-            numb_convert = dict_numbers[i]
-            if result < numb_convert * 5:
-                result = result + numb_convert
-            else:
-                result = result - numb_convert
-        return result
-    else:
+    dic =  {"I": 1, "V": 5, "X": 10, "L": 50,"C": 100, "D": 500, "M": 1000}
+    result = 0
+    item = 0
+    roman = roman_string
+    if type(roman) is not str or len(roman) is 0:
         return 0
+    for item in range(item, len(roman)):
+        if item < len(roman) - 1 and dic[roman[item]] < dic[roman[item + 1]]:
+            result -= dic[roman[item]]
+        else:
+            result += dic[roman[item]]
+    return result
