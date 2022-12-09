fc3db24b3917d3e4713cc63671e05754206d8adb
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-09 18:33:45 -0500

    Pass 'pycodestyle' checker

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,10 @@ def roman_to_int(roman_string):
     if isinstance(roman_string, str):
         result = 0
         numb_convert = 0
-        dict_nums = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, \
-                      "M": 1000, "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900 }
+        dict_nums = {
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,
+            "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900
+        }
         for i in reversed(roman_string):
             numb_convert = dict_nums[i]
             if result < numb_convert * 5:
