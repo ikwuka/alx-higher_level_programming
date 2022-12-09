e5fe43b8bc71a768f87ccfc7d13718867ae996e1
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-09 18:25:21 -0500

    Minimize whitespace

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    
     # @roman_string - returns the roman numeral of an integer
     if isinstance(roman_string, str):
         result = 0
@@ -9,7 +8,6 @@ def roman_to_int(roman_string):
                         "C": 100, "D": 500, "M": 1000, "IV": 4, \
                         "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900
                         }
-        
         for i in reversed(roman_string):
             numb_convert = dict_numbers[i]
             if result < numb_convert * 5:
