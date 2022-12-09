51921c9c3002b6cbd5ded199beb26ea075419521
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-09 18:03:31 -0500

    Change algorithm

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str):
-        result = 0
-        numb_convert = 0
-        dict_numbers = {"I": 1, "V": 5, "X": 10, "L": 50, \
-                        "C": 100, "D": 500, "M": 1000, "IV": 4, \
-                        "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900
-                        }
-        
-        for i in reversed(roman_string):
-            numb_convert = dict_numbers[i]
-            if result < numb_convert * 5:
-                result = result + numb_convert
-            else:
-                result = result - numb_convert
-        return result
+    
+    dict_numbers = {"I": 1, "V": 5, "X": 10, "L": 50, \
+                    "C": 100, "D": 500, "M": 1000, "IV": 4, \
+                    "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900
+                    }
+    
+    if roman_string and type(roman_string) == str:
+        num = 0
+        for i in range(len(roman_string)):
+            for j in dict_numbers.keys():
+                if roman_string[:] is j:
+                    num = dict_numbers[j]
+                    return num
+                if roman_string[i:i+1] is j:
+                    num += dict_numbers[j]
+                    break
+                
+        return num
     else:
         return 0
