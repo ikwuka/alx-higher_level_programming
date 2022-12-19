240afd056ad432eb5da2a4f1ade9c5d0bce45c47
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-19 14:36:31 -0500

    Execute a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:    
+        res = fct(*args)
+        return res
+    except Exception as err:
+        sys.stdout.write("Exceptioon: " + str(err) + "\n")
+        return
