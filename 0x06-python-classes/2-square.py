cfbbae977555d2a3f00e340477d7113d2ec3b988
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-31 04:18:42 -0500

    Define a square based on '1-square.py'

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Define square"""
+
+    def __init__(self, size=0):
+        """Constructor
+
+        Args:
+            size: length of square's side
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
