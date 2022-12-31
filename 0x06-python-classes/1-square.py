5e9b8a44ea93363268c8ca77981508e62dce07fb
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-31 04:08:36 -0500

    Refactor '1-square.py' code

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Square module"""
+
+
 class Square:
-    """
-    Add a private instance attribute to the class
-    """
+    """Define a square"""
+    
     def __init__(self, __size):
+        """Constructorr. 
+        
+        Args:
+            size: length of square's side.
+        """
         self.__size = __size
