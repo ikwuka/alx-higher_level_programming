4c20cb608c693f9b4a6195aae196d7336cb9d2d7
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2023-01-10 10:35:43 -0500

    Meet pycodestyle standard, write and append to a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,7 +12,7 @@ def read_file(filename=""):
     Raises:
         Exception: when the file can be opened
     """
-    
+
     with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
