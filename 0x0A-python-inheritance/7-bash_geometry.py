13e6f23ee441f9a59a21e01cbceea3802066e2ec
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2023-01-10 05:13:13 -0500

    Meet 'pycodestyle' standard

--- 0x0A-python-inheritance/7-bash_geometry.py
+++ 0x0A-python-inheritance/7-bash_geometry.py
@@ -4,26 +4,25 @@
 
 class BaseGeometry:
     """ Represent BaseGeometry """
-    
+
     def area(self):
         """ Yet to be implemented """
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """
         Validate a parameter as an integer
-        
+
         Args:
             name(str): The name of the parameter.
             value(int): The parameter to be validated
-            
+
         Raises:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-        
+
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        
